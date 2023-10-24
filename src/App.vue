355fd75a78ff1114ad7b636f0ea219a0a4eb0fe4
Аймбетова Балнур Сериковна <AymbetovaB@lcoast.itstep>
2023-10-24 17:13:13 +0600

    практика с axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <script>
+import axios from "axios";
 export default {
   data: () => {
-    return{}
+    return {
+      info: null,
+    };
   },
   computed: {},
-  watch: {},
-  methods: {}
-}
+  watch: {
+  },
+  methods: {},
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => {
+        this.info = response.data.bpi
+        console.log(this.info);
+      });
+  },
+};
 </script>
 
 <template>
-  <div class='app'>авлмвамиыамльвсьм</div>
+  <div id="app">
+    <div class='app--inner' v-for="item, index in info" :key="index">
+      <h1>Bitcoin Price Index</h1>
+      <p>Code: <span>{{item?.code}}</span></p>
+      <p>Description: <span>{{item?.description}}</span></p>
+      <p>Rate: <span>{{item?.rate}}</span></p>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#app{
+  display: flex;
+  gap: 50px;
+}
+.app--inner{
+  width: 300px;
+  border:1px solid black;
+  text-align: center;
+}
+</style>
