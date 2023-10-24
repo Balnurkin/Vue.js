51215518d971fada3799d828bc19b3b6495b7f40
Аймбетова Балнур Сериковна <AymbetovaB@lcoast.itstep>
2023-10-24 15:21:03 +0600

    Balnur: 24.10.2023 hw5

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script>
+import Slider from './components/Slider.vue'
 export default {
+  components:{
+    Slider,
+  },
   data: () => {
-    return{}
+    return{
+      slides:[
+        {title: 'Заголовок Slide One', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {title: 'Заголовок Slide Two', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {title: 'Заголовок Slide Three', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {title: 'Заголовок Slide Four', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+      ]
+    }
   },
   computed: {},
   watch: {},
@@ -10,7 +21,11 @@ export default {
 </script>
 
 <template>
-  <div class='app'>App</div>
+  <div class='app'>
+    <button>Previos</button>
+    <Slider />
+    <button>Next</button>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props:['id', 'title', 'text'],
+    data: () => {
+        return{}
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>
